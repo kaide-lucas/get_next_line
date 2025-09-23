e172f6f82190a77e342d9f1d1955724259d75c66
kaide-lucas <76270182+kaide-lucas@users.noreply.github.com>
2025-09-23 19:21:34 +0000

    New function: strlcpy

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_utilis.c                             :+:      :+:    :+:   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 22:30:40 by codespace         #+#    #+#             */
-/*   Updated: 2025/09/15 20:31:54 by codespace        ###   ########.fr       */
+/*   Updated: 2025/09/23 18:55:58 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,32 +17,43 @@ ft_strlen:
 Counts the length os the strings 's' (excluding the null terminator).
 Returns the number of characters.
 */
-size_t  ft_strlen(const char *s)
+size_t	ft_strlen(const char *s)
 {
-	size_t  i;
+	size_t	i;
 
 	i = 0;
-	while(s[i])
+	while (s[i])
 		i++;
 	return (i);
 }
+
 /*
 ft_strchr:
 Searches for the first occurrence of the character 'c' in the string 's'.
 Returns a pointer to the character if found, or NULL otherwise.
 */
-char	ft_strchr(const char *s, int c)
+char	*ft_strchr(const char *s, int c)
 {
-	while(*s)
+	int				i;
+	unsigned char	uc;
+
+	i = 0;
+	uc = (unsigned char)c;
+	while (s[i] != '\0')
 	{
-		if (*s == (char)c)
-			return ((char *)s);
-		s++;
+		if (s[i] == uc)
+		{
+			return ((char *)&s[i]);
+		}
+		i++;
 	}
-	if (*s == (char *)s);
-		return ((char *)s);
+	if (uc == '\0')
+	{
+		return ((char *)&s[i]);
+	}
 	return (NULL);
 }
+
 /*
 ft_strdup:
 Allocates sufficient memory and creates a copy of the string 's1'.
@@ -67,6 +78,7 @@ char	*ft_strdup(const char *s1)
 	dup[i] = '\0';
 	return (dup);
 }
+
 /*
 ft_strjoin:
 Concatenates 's1' and 's2' into a new string.
@@ -78,7 +90,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*res;
 	size_t	i;
 	size_t	j;
-	
+
 	if (!s1 || !s2)
 		return (NULL);
 	res = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
@@ -95,4 +107,31 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		res[i++] = s2[j++];
 	res[i] = '\0';
 	return (res);
+}
+
+/*
+ft_strlcpy:
+Copies up to (size - 1) characters from the string 'origin' to 'dest', 
+ensuring the result is null-terminated. Returns the length of 'origin'. 
+This function helps prevent buffer overflows during string copy operations.
+*/
+size_t	ft_strlcpy(char *dest, const char *origin, size_t size)
+{
+	size_t	i;
+	size_t	count;
+
+	i = 0;
+	count = 0;
+	if (size > 0)
+	{
+		while (i < size - 1 && origin[i] != '\0')
+		{
+			dest[i] = origin[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	while (origin[count] != '\0')
+		count++;
+	return (count);
 }
